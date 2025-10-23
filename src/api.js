b123a5c5104be827fc6e78cee5930e190def234a
luisixc <luislima.ixc@gmail.com>
2025-10-23 08:54:03 -0600

    add front

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Define tu API base por variable de entorno en el deploy (o edítalo localmente)
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3000'
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://v7f1ud08t1.execute-api.us-east-1.amazonaws.com'
 
 export const api = axios.create({
   baseURL: API_BASE,
