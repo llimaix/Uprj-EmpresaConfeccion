1044f0f97a01397be31b39a8c82965fb05ce95e3
Luis Lima <158865846+llimaix@users.noreply.github.com>
2025-10-24 02:46:00 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "https://v7f1ud08t1.execute-api.us-east-1.amazonaws.com";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://cg571d3qn5.execute-api.us-east-1.amazonaws.com";
 
 export const api = axios.create({
   baseURL: API_BASE,
   timeout: 15000,
 });
+
